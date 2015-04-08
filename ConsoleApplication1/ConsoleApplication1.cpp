b9f6e6403b75ea3b68bae4b3ecfddf4d7d422a0d
Jason McDonald <jason.mcdonald@cadre5.com>
2015-04-08 11:30:49 -0400

    starting camera dll

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ptgycam.h"
+#include <stdio.h>
+#include "oneframe.h"
+
+#if defined(WIN32) || defined(WIN64)
+#define _CRT_SECURE_NO_WARNINGS		
+#endif
+
+
+int main(int argc, char** argv)
+{
+	OneFrame test = OneFrame();
+	test.RecordVideo();
+
+	//RecordVideo();
+	//getchar();
+
+	return 0;
+}
+
